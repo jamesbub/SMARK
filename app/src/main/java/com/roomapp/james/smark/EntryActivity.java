24ffc35bb189c604c27243afe2e77c4856605ecc
jamesbub <biswajeetrout326@gmail.com>
2017-11-26 01:33:36 +0530

    Initial Commit

---
+++ app/src/main/java/com/roomapp/james/smark/EntryActivity.java
@@ -0,0 +1,32 @@
+package com.roomapp.james.smark;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+
+        Intent launchIntent = new Intent();
+        Class<?> launchActivity;
+        if(true)
+        {
+            launchActivity = LoginActivity.class;
+        }
+        else
+        {
+            launchActivity = MainActivity.class;
+        }
+        launchIntent.setClass(getApplicationContext(), launchActivity);
+        startActivity(launchIntent);
+
+        finish();
+    }
+
+}
+
